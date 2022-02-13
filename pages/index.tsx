328bcaa97d8b6c05b44f211a18febcf6e0f4e592
Emily Charles <emily.w.charles@gmail.com>
2022-02-12 21:49:53 -0500

    proof of concept connection to server

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,25 @@
 import type { NextPage } from "next";
 import Head from "next/head";
+import { useEffect, useState } from "react";
+
 import styles from "../styles/Home.module.scss";
 
+const serverUrl = process.env.NEXT_PUBLIC_SERVER_URL ?? "noServerUrlProvided";
 const Home: NextPage = () => {
+  const [loading, setLoading] = useState(false);
+  const [scents, setScents] = useState<any[]>([]);
+
+  useEffect(() => {
+    fetch(new URL(`${serverUrl}/scents/`).toString())
+      .then(async (res) => {
+        const result = await res.json();
+        setScents(result);
+        setLoading(false);
+      })
+      .catch((err) => {
+        console.error(err);
+      });
+  }, []);
   return (
     <div className={styles.container}>
       <Head>
@@ -14,6 +31,17 @@ const Home: NextPage = () => {
         <h1 className={styles.title}>Welcome to Common Scents!</h1>
 
         <p className={styles.description}>This is a work in progress.</p>
+        {!loading && (
+          <div>
+            {scents.map((scent, index) => {
+              return (
+                <div key={`scent-${index}`} className="scent">
+                  Scent {scent.name} is from house {scent.house_id}.
+                </div>
+              );
+            })}
+          </div>
+        )}
       </main>
 
       <footer className={styles.footer}>
